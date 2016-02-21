62433e7b0525d49dd5e2a88fc751cb22c422258f
Евгений <estaroselsky@rubiconware.com>
2016-02-21 23:23:09 +0200

    setup project

---
+++ InstaFeed/InstaFeed/InstaFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaFeed
+//
+//  Created by Евгений on 2/21/16.
+//  Copyright © 2016 Евгений. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaFeed/InstaFeed/InstaFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstaFeed
+//
+//  Created by Евгений on 2/21/16.
+//  Copyright © 2016 Евгений. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
